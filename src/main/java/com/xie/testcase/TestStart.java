0162140f0000370e5d843145f05aeff9a70e13f0
wang.chunjiang <wcjxxl@gmail.com>
2018-12-13 15:33:50 +0800

    Change to druid

--- src/main/java/com/xie/testcase/TestStart.java
+++ src/main/java/com/xie/testcase/TestStart.java
@@ -10,6 +10,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.jayway.jsonpath.JsonPath;
 import com.xie.beans.ParamBeans;
+import com.xie.dataprovider.DataProvider;
 import com.xie.tools.DBtools;
 import com.xie.util.HttpUtil;
 
@@ -17,49 +18,41 @@ public class TestStart {
 
 	@Test
 	public static void testStart() {
+		// List<NameValuePair> params = new ArrayList<NameValuePair>();
 		String token = "";
 		String expJson = "";
 		String result = "";
 		String entity = "";
 		String id = "";
-		String userId = "";
-		JsonObject returnData = new JsonObject();
+		JsonObject returnData=new JsonObject();
+		DataProvider dataProvider = new DataProvider();
 		HttpUtil httpUtil = new HttpUtil();
 		DBtools dBtools = new DBtools();
 		SqlSession session = dBtools.getSession("mybatis1.cfg.xml");
 		List<ParamBeans> list = session.selectList("mymapper1.selecttestcase");
 		for (int i = 0; i < list.size(); i++) {
-			entity = list.get(i).getParams();
-			expJson = list.get(i).getExp_json();
-
-			if (entity != null) {
-				returnData = new JsonParser().parse(entity).getAsJsonObject();// 把string类型的参数转成json格式
-				if (list.get(i).getIs_need_token() == 1) {
-					result = httpUtil.post(list.get(i).getUrl(), token,
-							returnData);
-				} else {
-					// 执行post，获取返回值
-					result = httpUtil.post(list.get(i).getUrl(), returnData);
-				}
-				if (result.contains("token")) {
-					token = JsonPath.read(result, "$.result.token");
-					expJson = expJson.replace("tihuan_token", token);
-				}
-				if (result.contains("id")) {
-					id = JsonPath.read(result, "$.result.id");
-					expJson = expJson.replace("tihuan_id", id);
-				}
-				if (result.contains("userId")) {
-					userId = JsonPath.read(result, "$.result.userId");
-					expJson = expJson.replace("tihuan_userId", userId);
-				}
+			entity = list.get(i).getParams();			
+			returnData = new JsonParser().parse(entity)
+					.getAsJsonObject();//把string类型的参数转成json格式
+			if (list.get(i).getIs_need_token() == 1) {
+			//	entity = entity + "&token=" + dataProvider.getToken();
+				result = httpUtil.post(list.get(i).getUrl(), dataProvider.getToken(),returnData);
 			} else {
-				result = httpUtil.post(list.get(i).getUrl(), token);
+				// 执行post，获取返回值
+				result = httpUtil.post(list.get(i).getUrl(), returnData);
 			}
-			JsonObject a = new JsonParser().parse(result).getAsJsonObject();
-			JsonObject b = new JsonParser().parse(expJson).getAsJsonObject();
-			Assert.assertEquals(a, b);
-			System.out.println("接口:" + list.get(i).getRemarks() + "测试成功");
+			expJson = list.get(i).getExp_json();
+			if (result.contains("token")) {
+				token = JsonPath.read(result, "$.result.token");
+				expJson = expJson.replace("tihuan", token);
+			}
+			if (result.contains("id")) {
+				id = JsonPath.read(result, "$.result.id");
+				expJson = expJson.replace("tihuan", id);
+			}
+			Assert.assertEquals(result, expJson);
+			
+			System.out.println("接口" + i + "测试成功");
 		}
 	}
 }
